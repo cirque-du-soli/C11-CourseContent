64e9f4b1eb74d0a75d6492c66b819c65f116c6f5
Soliloquy Yarrow <soliloquy.yarrow@gmail.com>
2023-09-13 19:53:07 -0400

    update to 17 from 20 (JDK)

---
+++ Projects/SY-ADMIN/src/main/java/com/solifsd08/sy/admin/SYADMIN.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.solifsd08.sy.admin;
+
+/**
+ *
+ * @author solil
+ */
+public class SYADMIN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+    }
+}
