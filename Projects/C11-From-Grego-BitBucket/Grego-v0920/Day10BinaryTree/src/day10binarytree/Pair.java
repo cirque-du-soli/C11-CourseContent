cfa46376be0f77a07cee708d98d48a1256eb4c4c
cirque-du-soli <soliloquy.yarrow@gmail.com>
2023-09-20 20:09:40 -0400

    ouuuf

---
+++ Projects/C11-From-Grego-BitBucket/Grego-v0920/Day10BinaryTree/src/day10binarytree/Pair.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package day10binarytree;
+
+/**
+ *
+ * @author grego
+ */
+public class Pair<K, V> {
+    public K key;
+    public V value;
+}
